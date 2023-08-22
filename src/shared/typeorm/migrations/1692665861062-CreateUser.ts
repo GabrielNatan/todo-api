9aadddd551829d4196a097067e35c30d81810fcf
gabriel natan <gabriel--natan@live.com>
2023-08-22 00:01:00 -0300

    feat: create migration create user

---
+++ src/shared/typeorm/migrations/1692665861062-CreateUser.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUser1692665861062 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'user',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'first_name',
+            type: 'varchar',
+          },
+          {
+            name: 'last_name',
+            type: 'varchar',
+          },
+          {
+            name: 'full_name',
+            type: 'varchar',
+          },
+          {
+            name: 'organization_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'OrganizationUser',
+            referencedTableName: 'organization',
+            referencedColumnNames: ['id'],
+            columnNames: ['organization_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('user');
+  }
+}
