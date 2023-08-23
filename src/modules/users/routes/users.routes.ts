74a13fffc477d332b60ad00f88774bcb8ea0d24c
gabriel natan <gabriel--natan@live.com>
2023-08-23 20:12:40 -0300

    feat: Create route post to user

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import UserController from '../controller/UserController';
+
+const usersRouter = Router();
+const usersController = new UserController();
+
+usersRouter.post('/', usersController.create);
+
+export default usersRouter;
