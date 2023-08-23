bfa20d156463074ce93cd4ba199e79c348590d20
gabriel natan <gabriel--natan@live.com>
2023-08-23 20:14:20 -0300

    feat: Add use route in routes settings

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,10 @@
 import organizationRoutes from '@modules/organizations/routes/organization.routes';
+import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
 routes.use('/organization', organizationRoutes);
+routes.use('/user', usersRouter);
 
 export default routes;
