2032397ec0efd2335339c00ca89887174ac7ab21
gabriel natan <gabriel--natan@live.com>
2023-08-22 22:04:22 -0300

    feat: create routes to show, update and delete organization.

--- src/modules/organizations/routes/organization.routes.ts
+++ src/modules/organizations/routes/organization.routes.ts
@@ -5,7 +5,12 @@ const organizationRoutes = Router();
 const organizationController = new OrganizationController();
 
 organizationRoutes.get('/', organizationController.index);
+organizationRoutes.get('/:id', organizationController.show);
 
 organizationRoutes.post('/', organizationController.create);
+
+organizationRoutes.put('/:id', organizationController.update);
+
+organizationRoutes.delete('/:id', organizationController.delete);
 
 export default organizationRoutes;
