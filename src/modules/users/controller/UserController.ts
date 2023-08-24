01ae565cd2f804c66f5779eb24908aa1356b69e4
gabriel natan <gabriel--natan@live.com>
2023-08-23 21:50:22 -0300

    feat: Add method index User controller

--- src/modules/users/controller/UserController.ts
+++ src/modules/users/controller/UserController.ts
@@ -1,13 +1,23 @@
 import { Request, Response, response } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ShowUserService from '../services/ShowUserService';
+import ListUserService from '../services/ListUserService';
+import UpdateUserService from '../services/UpdateUserService';
 
 class UserController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const showUserService = new ListUserService();
+
+    const users = await showUserService.execute();
+
+    return response.json(users);
+  }
+
   public async show(request: Request, response: Response): Promise<Response> {
     const { id } = request.params;
     const showUserService = new ShowUserService();
 
-    const user = await showUserService.execte(id);
+    const user = await showUserService.execute(id);
 
     return response.json(user);
   }
@@ -29,6 +39,23 @@ class UserController {
       full_name,
       email,
       organization_id,
+      password,
+    });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const { first_name, last_name, email, password } = request.body;
+
+    const updateUserService = new UpdateUserService();
+
+    const user = await updateUserService.execute({
+      id,
+      first_name,
+      last_name,
+      email,
       password,
     });
 
