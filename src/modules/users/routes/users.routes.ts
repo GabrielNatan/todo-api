b2d9d0199ed9e60739ca44744b82192d856e58c6
gabriel natan <gabriel--natan@live.com>
2023-08-23 21:50:44 -0300

    feat: Add route for list users

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -4,7 +4,9 @@ import UserController from '../controller/UserController';
 const usersRouter = Router();
 const usersController = new UserController();
 
+usersRouter.get('/', usersController.index);
 usersRouter.get('/:id', usersController.show);
 usersRouter.post('/', usersController.create);
+usersRouter.put('/:id', usersController.update);
 
 export default usersRouter;
