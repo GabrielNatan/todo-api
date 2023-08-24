c62991f83a8c061f6bc409b49be3d71ac1c08941
gabriel natan <gabriel--natan@live.com>
2023-08-23 21:18:19 -0300

    feat: Add show method and add password on create user

--- src/modules/users/controller/UserController.ts
+++ src/modules/users/controller/UserController.ts
@@ -1,10 +1,26 @@
 import { Request, Response, response } from 'express';
 import CreateUserService from '../services/CreateUserService';
+import ShowUserService from '../services/ShowUserService';
 
 class UserController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const { id } = request.params;
+    const showUserService = new ShowUserService();
+
+    const user = await showUserService.execte(id);
+
+    return response.json(user);
+  }
+
   public async create(request: Request, response: Response): Promise<Response> {
-    const { first_name, last_name, full_name, email, organization_id } =
-      request.body;
+    const {
+      first_name,
+      last_name,
+      full_name,
+      email,
+      organization_id,
+      password,
+    } = request.body;
     const createUserService = new CreateUserService();
 
     const user = await createUserService.execute({
@@ -13,6 +29,7 @@ class UserController {
       full_name,
       email,
       organization_id,
+      password,
     });
 
     return response.json(user);
