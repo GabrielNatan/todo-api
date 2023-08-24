f3eb0919b328998d703867de7d278436d0d85c51
gabriel natan <gabriel--natan@live.com>
2023-08-23 21:18:40 -0300

    feat: Create show user route

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -4,6 +4,7 @@ import UserController from '../controller/UserController';
 const usersRouter = Router();
 const usersController = new UserController();
 
+usersRouter.get('/:id', usersController.show);
 usersRouter.post('/', usersController.create);
 
 export default usersRouter;
