eeeff789d832184ae4066acffc1d11a1445ce563
gabriel natan <gabriel--natan@live.com>
2023-08-24 21:29:19 -0300

    feat: Using task routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,5 @@
 import organizationRoutes from '@modules/organizations/routes/organization.routes';
+import taskRoutes from '@modules/tasks/routes/task.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
@@ -6,5 +7,6 @@ const routes = Router();
 
 routes.use('/organization', organizationRoutes);
 routes.use('/user', usersRouter);
+routes.use('/task', taskRoutes);
 
 export default routes;
