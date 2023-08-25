e7ec8797e729685fc927efa7fec91bcebd8ae861
gabriel natan <gabriel--natan@live.com>
2023-08-24 22:56:55 -0300

    feat: add route updateUserTask

--- src/modules/tasks/routes/task.routes.ts
+++ src/modules/tasks/routes/task.routes.ts
@@ -8,6 +8,7 @@ taskRoutes.get('/', taskController.index);
 taskRoutes.get('/:id', taskController.show);
 taskRoutes.post('/', taskController.create);
 taskRoutes.put('/:id', taskController.update);
+taskRoutes.put('/users/:id', taskController.updateUserTask);
 taskRoutes.delete('/:id', taskController.delete);
 
 export default taskRoutes;
