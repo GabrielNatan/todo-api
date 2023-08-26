ada4c105654c046b1ad618fdae5e416e1c688985
gabriel natan <gabriel--natan@live.com>
2023-08-26 12:50:18 -0300

    feat: add celebrate in organizationRoutes

--- src/modules/organizations/routes/organization.routes.ts
+++ src/modules/organizations/routes/organization.routes.ts
@@ -1,15 +1,41 @@
 import { Router } from 'express';
 import OrganizationController from '../controllers/OrganizationController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const organizationRoutes = Router();
 const organizationController = new OrganizationController();
 
 organizationRoutes.get('/', organizationController.index);
-organizationRoutes.get('/:id', organizationController.show);
 
-organizationRoutes.post('/', organizationController.create);
+organizationRoutes.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.string().required(),
+    }),
+  }),
+  organizationController.show,
+);
 
-organizationRoutes.put('/:id', organizationController.update);
+organizationRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+    },
+  }),
+  organizationController.create,
+);
+
+organizationRoutes.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  organizationController.update,
+);
 
 organizationRoutes.delete('/:id', organizationController.delete);
 
