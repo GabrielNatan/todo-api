faae7deb19a645716e418eb297dd27dad4ae8c87
gabriel natan <gabriel--natan@live.com>
2023-08-28 23:06:48 -0300

    feat: change full name in User controller

--- src/modules/users/controller/UserController.ts
+++ src/modules/users/controller/UserController.ts
@@ -23,20 +23,14 @@ class UserController {
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    const {
-      first_name,
-      last_name,
-      full_name,
-      email,
-      organization_id,
-      password,
-    } = request.body;
+    const { first_name, last_name, email, organization_id, password } =
+      request.body;
     const createUserService = new CreateUserService();
 
     const user = await createUserService.execute({
       first_name,
       last_name,
-      full_name,
+      full_name: `${first_name} ${last_name}`,
       email,
       organization_id,
       password,
