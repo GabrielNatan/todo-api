a9d093e6db4da8a3b286b813ad5df5c826c17fc0
gabriel natan <gabriel--natan@live.com>
2023-08-28 23:07:09 -0300

    feat: add celebrate in users routes

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,12 +1,48 @@
 import { Router } from 'express';
 import UserController from '../controller/UserController';
+import { Segments, celebrate, Joi } from 'celebrate';
 
 const usersRouter = Router();
 const usersController = new UserController();
 
 usersRouter.get('/', usersController.index);
-usersRouter.get('/:id', usersController.show);
-usersRouter.post('/', usersController.create);
-usersRouter.put('/:id', usersController.update);
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.show,
+);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      first_name: Joi.string().required(),
+      last_name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      organization_id: Joi.string().uuid().required(),
+      password: Joi.string().min(8).max(30).required(),
+    },
+  }),
+  usersController.create,
+);
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      first_name: Joi.string(),
+      last_name: Joi.string(),
+      email: Joi.string().email(),
+      password: Joi.string().min(8).max(30),
+    },
+  }),
+  usersController.update,
+);
 
 export default usersRouter;
