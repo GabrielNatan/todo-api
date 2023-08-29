e0e2b89d2b0dbed16ae94f14225e2dd9dc3ea2fe
gabriel natan <gabriel--natan@live.com>
2023-08-28 23:50:09 -0300

    feat: using session routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,6 @@
 import organizationRoutes from '@modules/organizations/routes/organization.routes';
 import taskRoutes from '@modules/tasks/routes/task.routes';
+import sessionRoutes from '@modules/users/routes/session.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
 
@@ -8,5 +9,6 @@ const routes = Router();
 routes.use('/organization', organizationRoutes);
 routes.use('/user', usersRouter);
 routes.use('/task', taskRoutes);
+routes.use('/session', sessionRoutes);
 
 export default routes;
