e53bb7a62f0eb9ae30512bcf6280aa6b47546a7d
gabriel natan <gabriel--natan@live.com>
2023-08-28 23:06:12 -0300

    feat: add celebrate in task routes

--- src/modules/tasks/routes/task.routes.ts
+++ src/modules/tasks/routes/task.routes.ts
@@ -1,14 +1,60 @@
 import { Router } from 'express';
 import TaskController from '../controller/TaskController';
+import { Segments, celebrate, Joi } from 'celebrate';
 
 const taskRoutes = Router();
 const taskController = new TaskController();
 
 taskRoutes.get('/', taskController.index);
-taskRoutes.get('/:id', taskController.show);
-taskRoutes.post('/', taskController.create);
-taskRoutes.put('/:id', taskController.update);
-taskRoutes.put('/users/:id', taskController.updateUserTask);
-taskRoutes.delete('/:id', taskController.delete);
+taskRoutes.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  taskController.show,
+);
+taskRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      title: Joi.string().required(),
+      message: Joi.string().required(),
+    },
+  }),
+  taskController.create,
+);
+taskRoutes.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      title: Joi.string(),
+      message: Joi.string(),
+    },
+  }),
+  taskController.update,
+);
+taskRoutes.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  taskController.delete,
+);
+taskRoutes.put(
+  '/users/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  taskController.updateUserTask,
+);
 
 export default taskRoutes;
